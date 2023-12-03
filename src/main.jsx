64184d93b2a2d143c76e571e9c820531d3a95144
Brian <picketfence14@gmail.com>
2023-12-03 12:14:03 -0500

    Quick Redux setup finished

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,46 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createStore } from 'redux';
+
+
+// STORE ACTION
+const increment = () => {
+  return {
+    type: 'INCREMENT',
+
+
+  };
+};
+
+// REDUCER
+const counterReducer = ( state = 0, action ) => {
+  switch(action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+  }
+}
+
+// STORE
+let store = createStore(counterReducer);
+
+
+
+
+
+// DISPATCH (FIRE!!!)
+
+
+ReactDOM.render(
+
+  document.getElementById('root')).render(
+    store.subscribe( () => console.log( store.getState() ) ) )
+    store.dispatch( increment() );
+
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+
 )
