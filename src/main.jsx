477cf47165c71c746b26491b69d9955078b0eba0
Brian Smith <briansmith@rocketcentral.com>
2023-12-04 16:12:30 -0500

    Homepage is rendering

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 //import { store } from 'store/reduxStore'
-import { configureStore } from '@reduxjs/toolkit'
+import { createStore, configureStore } from '@reduxjs/toolkit'
 import './index.scss'
 import allReducers from './reducers'
 import { createRoot } from 'react-dom/client'
 
-const store = configureStore({ reducer: allReducers })
+const store = createStore(
+	allReducers,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+ )
+
+// const store = configureStore({ 
+// 	reducer: allReducers,
+// 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//  })
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Provider store={store}><App /></Provider>);
